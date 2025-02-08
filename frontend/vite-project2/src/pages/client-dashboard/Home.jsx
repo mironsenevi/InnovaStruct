acf01570c2a6eba757cc624a4c79d4de592c2ccc
mironsenevi <senevirathnamiron@gmail.com>
2025-02-08 12:30:21 +0530

    updated
    installed tailwind v3, daisy UI.
    created with node 18(LTS)

---
+++ frontend/vite-project2/src/pages/client-dashboard/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ClientNavbar from '../../components/ClientNavbar'
+
+const Home = () => {
+  return (
+    <div>
+        <ClientNavbar />
+    </div>
+  )
+}
+
+export default Home
