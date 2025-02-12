0b7d72b2e630332b43e5ef7942ce0976c8072193
Saiganeshwaran <saiganeshwaran@gmail.com>
2025-02-12 13:11:28 +0530

    components 2

---
+++ frontend/vite-project2/src/components/LandingPageComponents/Footer.jsx
@@ -0,0 +1,26 @@
+import { FaInstagramSquare, FaFacebook, FaLinkedin } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer className="flex flex-col items-center text-xs text-zinc-400 mt-36">
+      {/* Social Media Icons with Links */}
+      <div className="flex gap-6 text-6xl text-yellow-400">
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <FaFacebook className="hover:text-blue-600 transition duration-300" />
+        </a>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <FaInstagramSquare className="hover:text-pink-500 transition duration-300" />
+        </a>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin className="hover:text-blue-700 transition duration-300" />
+        </a>
+      </div>
+
+      {/* Footer Text */}
+      <p className="mt-4">Built by Team Mantra Matrix</p>
+      <p className="mb-10">All rights reserved. ©</p>
+    </footer>
+  );
+}
+
+export default Footer;
