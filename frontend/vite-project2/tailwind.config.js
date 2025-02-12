7007c74631956885aef3a49c7a6e2e1606e6aad3
Saiganeshwaran <saiganeshwaran@gmail.com>
2025-02-12 13:17:24 +0530

    fundamental 4

---
+++ frontend/vite-project2/tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  important: true, // Add this line to prioritize custom CSS over Tailwind's utility classes
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Raleway': ["Poppins", "sans-serif"],
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#f59e0b",
+        },
+        innovastruct: {
+          "50": "#e6edf7",
+          "100": "#ccdbef",
+          "200": "#99b7de",
+          "300": "#6692ce",
+          "400": "#f59e0b",
+          "500": "#004aad",
+          "600": "#003b8a",
+          "700": "#002c68",
+          "800": "#001e45",
+          "900": "#000f23"
+        }
+      },                    
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+}
