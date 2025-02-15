eddd4c75ec6044fb478a7df9fa1a7ab382a1967d
Omindu Abeywardane <omindugeemitha4488@gmail.com>
2025-02-15 08:13:28 +0530

    Settings for User

---
+++ frontend/vite-project2/src/pages/client-dashboard/Settings.jsx
@@ -0,0 +1,12 @@
+// filepath: src/pages/client-dashboard/Settings.jsx
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <div>
+      <h1>Settings Page</h1>
+    </div>
+  );
+};
+
+export default Settings;
