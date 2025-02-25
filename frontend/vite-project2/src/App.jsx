af92d649e430d36e425011b2ca6935ee3406c899
DinayaJW <dinayayuhansi13@gmail.com>
2025-02-25 23:33:00 +0530

    add company nav bar

--- frontend/vite-project2/src/App.jsx
+++ frontend/vite-project2/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/client-dashboard/Home";
 import Login from "./pages/Authentication/Login";
+import CompanyHome from "./pages/company-dashboard/CompanyHome";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
+          <Route path="/company/home" element={<CompanyHome/>}/>
           <Route path="/client/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Routes>
---
+++ frontend/vite-project2/src/pages/company-dashboard/CompanyHome.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CompanyNavbar from 'e:/InnovaStruct/frontend/vite-project2/src/pages/company-dashboard/companyHome.jsx';
+
+const CompanyHome = () => {
+  return (
+    <div>
+        <CompanyNavbar/>
+    </div>
+  )
+}
+
+export default CompanyHome;
