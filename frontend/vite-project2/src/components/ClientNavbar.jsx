b5c542b1f47bdae922ff77b9842703a2548248d0
Omindu Abeywardane <omindugeemitha4488@gmail.com>
2025-02-26 14:18:12 +0530

    Improved version of client navbar
    colors changed

--- frontend/vite-project2/src/components/ClientNavbar.jsx
+++ frontend/vite-project2/src/components/ClientNavbar.jsx
@@ -12,11 +12,11 @@ import logo from "../assets/logo.png";
 
 function ClientNavbar() {
   return (
-    <nav className="fixed top-0 left-0 h-full w-0 sm:w-64 bg-yellow-400 text-white overflow-hidden transition-all duration-300 z-50">
+    <nav className="fixed top-0 left-0 h-full w-0 sm:w-64 bg-yellow-400 text-yellow-900 overflow-hidden transition-all duration-300 z-50">
       <div className="px-6 py-4 border-b border-yellow-300">
         <div className="flex items-center gap-4">
         <img src={logo} alt="InnovaStruct Logo" className="w-9 h-10.5 -translate-y-0.5 " /> 
-        <h2 className="text-2xl font-bold text-black" >InnovaStruct</h2>
+        <h2 className="text-2xl font-bold text-yellow-900" >InnovaStruct</h2>
         </div>
       </div>
       <ul className="flex flex-col p-4 space-y-4">
@@ -49,21 +49,19 @@ function ClientNavbar() {
         </li>
         <li>
           <Link
-            to="/client/tender"
+             to="/client/tender"
             className="flex items-center gap-3 p-2 hover:bg-white hover:text-black rounded-lg transition-colors"
           >
-            <FileText className="w-5 h-5" />
-            <span>Tender</span>
+          <FileText className="w-5 h-5" />
+          <span>Tenders</span>
           </Link>
         </li>
+        
         <li>
-          <Link
-            to="/client/settings"
-            className="flex items-center gap-3 p-2 hover:bg-white hover:text-black rounded-lg transition-colors"
-          >
-            <Settings className="w-5 h-5" />
-            <span>Settings</span>
-          </Link>
+        <Link to="/client/settings" className="flex items-center gap-3 p-2 hover:bg-white hover:text-black rounded-lg transition-colors">
+  <Settings className="w-5 h-5" />
+  <span>Settings</span>
+</Link>
         </li>
       </ul>
       <div className="absolute bottom-4 w-full px-4">
