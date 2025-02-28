cc83be9e2190903f9c4f4b0b30c794ca34c176f9
mironsenevi <senevirathnamiron@gmail.com>
2025-02-28 11:58:13 +0530

    initialize new springboot project

---
+++ backend/Innovastruct/src/main/java/com/innovastruct/Innovastruct/InnovastructApplication.java
@@ -0,0 +1,13 @@
+package com.innovastruct.Innovastruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovastructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovastructApplication.class, args);
+	}
+
+}
