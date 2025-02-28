809355e815a8041aeddd991dfedd89d6dfdf899e
mironsenevi <senevirathnamiron@gmail.com>
2025-02-28 15:24:10 +0530

    create TenderService class

---
+++ backend/Innovastruct/src/main/java/com/innovastruct/Innovastruct/services/TenderService.java
@@ -0,0 +1,29 @@
+package com.innovastruct.Innovastruct.services;
+
+
+
+import com.innovastruct.Innovastruct.models.Tender ;
+import com.innovastruct.Innovastruct.repositories.TenderRepository ;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class TenderService {
+    private final TenderRepository tenderRepository;
+
+    public TenderService(TenderRepository tenderRepository) {
+        this.tenderRepository = tenderRepository;
+    }
+
+    public Tender createTender(Tender tender) {
+        return tenderRepository.save(tender);
+    }
+
+    public List<Tender> getAllOpenTenders() {
+        return tenderRepository.findByStatus("open");
+    }
+
+    public Tender getTenderById(String id) {
+        return tenderRepository.findById(id).orElse(null);
+    }
+}
