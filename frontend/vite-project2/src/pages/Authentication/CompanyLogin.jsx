273ed9dae4afe426e46502074e967cabca9ef3f3
unknown <aadilaroos4@gmail.com>
2025-03-02 11:15:33 +0530

    Create company login component with basic structure

---
+++ frontend/vite-project2/src/pages/Authentication/CompanyLogin.jsx
@@ -0,0 +1,49 @@
+// src/pages/Authentication/CompanyLogin.jsx
+import React from "react";
+
+function CompanyLogin() {
+  return (
+    <div>
+      <div className="relative flex flex-col justify-center h-screen overflow-hidden">
+        <div className="w-full p-6 m-auto bg-white rounded-md shadow-md ring-2 ring-yellow-400 lg:max-w-lg">
+          <h1 className="text-3xl font-semibold text-center text-gray-700">
+            Company Login
+          </h1>
+          <form className="space-y-4">
+            <div>
+              <label className="label">
+                <span className="text-base label-text">Company Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Company Email Address"
+                className="w-full input input-bordered"
+              />
+            </div>
+            <div>
+              <label className="label">
+                <span className="text-base label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Enter Password"
+                className="w-full input input-bordered"
+              />
+            </div>
+            <a
+              href="#"
+              className="text-xs text-gray-600 hover:underline hover:text-blue-600"
+            >
+              Forget Password?
+            </a>
+            <div>
+              <button className="btn-neutral btn btn-block">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CompanyLogin;
