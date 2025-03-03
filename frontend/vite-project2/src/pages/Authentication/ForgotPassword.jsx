8314d011f83a8ffb0cad5dd02a76ab925cb45e91
unknown <aadilaroos4@gmail.com>
2025-03-03 23:15:34 +0530

    1. Add basic ForgotPassword component structure

---
+++ frontend/vite-project2/src/pages/Authentication/ForgotPassword.jsx
@@ -0,0 +1,12 @@
+// First commit - Basic component structure
+import React from 'react';
+
+function ForgotPassword() {
+  return (
+    <div>
+      <h1>Password Recovery</h1>
+    </div>
+  );
+}
+
+export default ForgotPassword;
