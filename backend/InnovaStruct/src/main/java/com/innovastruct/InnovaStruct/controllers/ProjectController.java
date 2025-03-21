2312016fa5a28f463892f7e600617e91f8dcd1a2
Dulain Perera <dulainp337@gmail.com>
2025-03-21 23:54:23 +0530

    controller fix

--- backend/InnovaStruct/src/main/java/com/innovastruct/InnovaStruct/controllers/ProjectController.java
+++ backend/InnovaStruct/src/main/java/com/innovastruct/InnovaStruct/controllers/ProjectController.java
@@ -4,10 +4,9 @@ import java.util.List;
 
 public class ProjectController {
     private int id;
-    private String name; // Renamed from projectName to match form field
+    private String name; 
     private String description;
-    private Integer completionYear; // New field from form, Integer type
-    private List<String> images; // List of image URLs (after upload)
-    // Removed startDate, endDate, projectLocation, clientName as they are not in the form
-
+    private Integer completionYear; 
+    private List<String> images; 
+    
 }
