b5fedd244a73ad115973e326a79ab4c1d391d85c
Dulain Perera <dulainp337@gmail.com>
2025-03-23 01:33:37 +0530

    Update ContactsTable.jsx

--- frontend/vite-project2/src/pages/Contacts/ContactsTable.jsx
+++ frontend/vite-project2/src/pages/Contacts/ContactsTable.jsx
@@ -3,9 +3,31 @@ import React from 'react'
 import React from 'react'
 
 const ContactsTable = () => {
-  return (
-    <div>ContactsTable</div>
-  )
-}
-
-export default ContactsTable
+    return (
+      <div>
+        <h1>Contacts Table</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map(contact => (
+              <tr key={contact.id}>
+                <td>{contact.id}</td>
+                <td>{contact.name}</td>
+                <td>{contact.email}</td>
+                <td>{contact.phone}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+  
+  export default ContactsTable;
