3e8cad07053f3e993885b71e822309ce8962f300
Dulain Perera <dulainp337@gmail.com>
2025-03-23 02:11:14 +0530

    Update ContactsTable.jsx

--- frontend/vite-project2/src/pages/Contacts/ContactsTable.jsx
+++ frontend/vite-project2/src/pages/Contacts/ContactsTable.jsx
@@ -4,7 +4,7 @@ const contacts = [
     { id: 1, name: 'John Doe', email: 'john.doe@example.com', phone: '123-456-7890' },
     { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com', phone: '987-654-3210' },
     { id: 3, name: 'Sam Johnson', email: 'sam.johnson@example.com', phone: '555-555-5555' },
-    { id: 4, name: 'Mike Jones', email: ' ' , phone: ' ' }          
+    { id: 4, name: 'Mike Jones', email: 'mike.jones ' , phone: ' 89074322' }          
 ];
 
 const ContactsTable = () => {
