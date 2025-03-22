922e9e0119227c4038f25555db503ee08be260c0
unknown <aadilaroos4@gmail.com>
2025-03-22 17:39:46 +0530

    chatbot chat messege file changed

--- frontend/vite-project2/src/components/chatbot/ChatMessage.jsx
+++ frontend/vite-project2/src/components/chatbot/ChatMessage.jsx
@@ -11,27 +11,7 @@ const ChatMessages = ({ messages }) => {
     scrollToBottom();
   }, [messages]);
 
-  return (
-    <div className="flex-1 overflow-y-auto p-4 space-y-4">
-      {messages.map((message, index) => (
-        <div
-          key={index}
-          className={`flex ${message.sender === 'user' ? 'justify-end' : 'justify-start'}`}
-        >
-          <div
-            className={`max-w-[70%] rounded-lg p-3 ${
-              message.sender === 'user'
-                ? 'bg-blue-500 text-white'
-                : 'bg-gray-200 text-gray-800'
-            }`}
-          >
-            {message.text}
-          </div>
-        </div>
-      ))}
-      <div ref={messagesEndRef} />
-    </div>
-  );
+  
 };
 
 export default ChatMessages;
