3230c29695a632aab5bf8a05147c81cceb6142a7
Dulain Perera <dulainp337@gmail.com>
2025-03-23 20:16:24 +0530

    Update userService.js

--- frontend/vite-project2/src/services/userService.js
+++ frontend/vite-project2/src/services/userService.js
@@ -1,1 +1,17 @@
 import api from './api';
+
+const userService = {
+    // Authentication
+    login: async (email, password) => {
+      try {
+        const response = await api.post('/users/login', { email, password });
+        if (response.data.token) {
+          localStorage.setItem('token', response.data.token);
+          localStorage.setItem('user', JSON.stringify(response.data.user));
+        }
+        return response.data;
+      } catch (error) {
+        throw error;
+      }
+    },
+  
